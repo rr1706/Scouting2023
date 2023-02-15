2bb60e55dc6adaf84998fed3e349548c4c25a885
scouting1706 <scouting1706@gmail.com>
2023-02-14 19:48:17 -0600

    I made updates to the new alliance scout thing - X

---
+++ allianceScout2023/app/src/main/java/com/frc1706/alliancescout2023/StartDragListener.java
@@ -0,0 +1,8 @@
+package com.frc1706.alliancescout2023;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface StartDragListener {
+    void requestDrag(RecyclerView.ViewHolder viewHolder);
+}
