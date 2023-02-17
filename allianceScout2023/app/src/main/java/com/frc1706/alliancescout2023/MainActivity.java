aaa20edaed1836c2ecde1312f55c4cf3493a7f55
scouting1706 <scouting1706@gmail.com>
2023-02-16 19:07:54 -0600

    Fully finished GUI on new alliance thing, working on functions now - X

--- allianceScout2023/app/src/main/java/com/frc1706/alliancescout2023/MainActivity.java
+++ allianceScout2023/app/src/main/java/com/frc1706/alliancescout2023/MainActivity.java
@@ -4,26 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 
+import android.os.Environment;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputEditText;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
@@ -31,10 +42,10 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
     Spinner alliance_sel;
-    View line1,line2,line3;
-    ConstraintLayout inputTeams;
-    EditText team1,team2,team3,scouter,match,round;
+    View line1,line2,line3,line4,line5,line6;
 
+    EditText team1,team2,team3,scouter,match;
+    TextView inputTeams;
 
     RecyclerView recyclerView;
     RecyclerViewAdapter mAdapter;
@@ -44,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Objects.requireNonNull(getSupportActionBar()).hide();
@@ -55,23 +67,82 @@ public class MainActivity extends AppCompatActivity {
         line1= findViewById(R.id.line1);
         line2= findViewById(R.id.line2);
         line3 = findViewById(R.id.line3);
+        line4= findViewById(R.id.line4);
+        line5= findViewById(R.id.line5);
+        line6 = findViewById(R.id.line6);
+        View[] lines = {line1,line2,line3,line4,line5,line6};
+        inputTeams = findViewById(R.id.inputTeamsTxt);
+
         submit = findViewById(R.id.submit);
+        team1 = findViewById(R.id.team1);
+        team2 = findViewById(R.id.team2);
+        team3 = findViewById(R.id.team3);
+        scouter = findViewById(R.id.name_input);
+        match = findViewById(R.id.round_input);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                0);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
 
         LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
         recyclerView.setHasFixedSize(true);
-
-
-
         populateRecyclerView();
 
+        getTeams();
+
+        if(!getTeams().equals("")) {
+            team1.setVisibility(View.GONE);
+            team2.setVisibility(View.GONE);
+            team3.setVisibility(View.GONE);
+            line4.setVisibility(View.GONE);
+            line5.setVisibility(View.GONE);
+            line6.setVisibility(View.GONE);
+            inputTeams.setVisibility(View.GONE);
+        }
+
+
+
+        match.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                hideKeyboard(v);
+            }
+        });
+        scouter.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                hideKeyboard(v);
+            }
+        });
+        team1.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                rankUpD();
+                hideKeyboard(v);
+            }
+        });
+        team2.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+
+                rankUpD();
+                hideKeyboard(v);
+            }
+        });
+        team3.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+
+                rankUpD();
+                hideKeyboard(v);
+            }
+        });
 
         alliance_sel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> arg0, View arg1,int arg2, long arg3) {
                 if (alliance_sel.getSelectedItem().toString().equals("BLUE")) {
-                    line1.setBackgroundColor(Color.BLUE);
-                    line2.setBackgroundColor(Color.BLUE);
-                    line3.setBackgroundColor(Color.BLUE);
+                    for (View line : lines) {
+                        line.setBackgroundColor(Color.BLUE);
+                    }
                     for(int i=0;i<3;i++) {
                         CardView relativeLayout = (CardView) recyclerView.getChildAt(i);
                         relativeLayout.setBackgroundColor(Color.BLUE);
@@ -81,9 +152,9 @@ public class MainActivity extends AppCompatActivity {
 
 
                 } else if (alliance_sel.getSelectedItem().toString().equals("RED")) {
-                    line1.setBackgroundColor(Color.RED);
-                    line2.setBackgroundColor(Color.RED);
-                    line3.setBackgroundColor(Color.RED);
+                    for (View line : lines) {
+                        line.setBackgroundColor(Color.RED);
+                    }
                     for(int i=0;i<3;i++) {
                         CardView relativeLayout = (CardView) recyclerView.getChildAt(i);
                         relativeLayout.setBackgroundColor(Color.RED);
@@ -108,6 +179,96 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private String getTeams() {
+
+        StringBuilder text = new StringBuilder();
+        try {
+            File documents = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            File file;
+            file = new File(documents + "/ScoutingTeams.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.e("log", text.toString());
+        return text.toString();
+    }
+    public void onBackPressed() {}
+
+    private File getDataDirectory() {
+        File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File myDir = new File(directory + "/ScoutingData");
+        myDir.mkdirs();
+        return myDir;
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+    public void rankUpD() {
+        if(!team1.getText().toString().equals("") && !team2.getText().toString().equals("") && !team3.getText().toString().equals("")) {
+            stringArrayList.set(0,team1.getText().toString());
+            stringArrayList.set(1,team2.getText().toString());
+            stringArrayList.set(2,team3.getText().toString());
+
+            mAdapter = new RecyclerViewAdapter(stringArrayList);
+            ItemTouchHelper.Callback callback = new ItemMoveCallback(mAdapter);
+            ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+            touchHelper.attachToRecyclerView(recyclerView);
+            recyclerView.setAdapter(mAdapter);
+        }
+    }
+
+    //@Override
+    //public boolean dispatchTouchEvent(MotionEvent event) {
+    //    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+    //        View v = getCurrentFocus();
+    //        if ( v instanceof EditText) {
+    //            Rect outRect = new Rect();
+    //            v.getGlobalVisibleRect(outRect);
+    //            if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+    //                if(!team1.getText().toString().equals("") && !team2.getText().toString().equals("") && !team3.getText().toString().equals("")) {
+    //                    if(team1.hasFocus()){
+    //                        stringArrayList.set(0,team1.getText().toString());
+    //                        stringArrayList.set(1,team2.getText().toString());
+    //                        stringArrayList.set(2,team3.getText().toString());
+    //                    }
+    //                    if(team2.hasFocus()){
+    //                        stringArrayList.set(0,team1.getText().toString());
+    //                        stringArrayList.set(1,team2.getText().toString());
+    //                        stringArrayList.set(2,team3.getText().toString());
+    //                    }
+    //                    if(team3.hasFocus()){
+    //                        stringArrayList.set(0,team1.getText().toString());
+    //                        stringArrayList.set(1,team2.getText().toString());
+    //                        stringArrayList.set(2,team3.getText().toString());
+    //                    }
+    //                    mAdapter = new RecyclerViewAdapter(stringArrayList);
+//
+    //                    ItemTouchHelper.Callback callback = new ItemMoveCallback(mAdapter);
+    //                    ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+    //                    touchHelper.attachToRecyclerView(recyclerView);
+//
+    //                    recyclerView.setAdapter(mAdapter);
+    //                }
+//
+//
+    //                v.clearFocus();
+    //                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    //                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    //            }
+    //        }
+    //    }
+    //    return super.dispatchTouchEvent( event );
+    //}
+
     private void populateRecyclerView() {
 
         stringArrayList.add("TEAM #1");
@@ -116,8 +277,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new RecyclerViewAdapter(stringArrayList);
 
-        ItemTouchHelper.Callback callback =
-                new ItemMoveCallback(mAdapter);
+        ItemTouchHelper.Callback callback = new ItemMoveCallback(mAdapter);
         ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
         touchHelper.attachToRecyclerView(recyclerView);
 
